ee295df58069c0c455edc9602c35e5d9c66c14db
zsigmondO <zsigmondo9000@gmail.com>
2020-05-05 17:33:09 +0200

    Categories fixed

--- src/main/resources/META-INF/data.sql
+++ src/main/resources/META-INF/data.sql
@@ -1,5 +1,13 @@
 INSERT INTO Category (id, name) VALUES (1,'baked_dish');
 INSERT INTO Category (id, name) VALUES (2,'beef');
+INSERT INTO Category (id, name) VALUES (3,'chicken');
+INSERT INTO Category (id, name) VALUES (4,'dinner');
+INSERT INTO Category (id, name) VALUES (5,'easy');
+INSERT INTO Category (id, name) VALUES (6,'healthy');
+INSERT INTO Category (id, name) VALUES (7,'kid_friendly');
+INSERT INTO Category (id, name) VALUES (8,'quick');
+INSERT INTO Category (id, name) VALUES (9,'vegetables');
+
 
 INSERT INTO Food (id, name, image, recipe) VALUES (1,'name',RAWTOHEX('Test'),'Tesztelő szöveg. Itt lesz a recept');
 INSERT INTO Food (id, name, image, recipe) VALUES (2,'name2',RAWTOHEX('Test2'),'Tesztelő szöveg kettes! Itt lesz a recept');
