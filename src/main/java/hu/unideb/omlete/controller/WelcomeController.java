0c3818b139f60a0269e0e6c917b4e493bc01e41b
ETamas123 <tamas.ermenyi1@gmail.com>
2020-05-08 18:03:29 +0200

    WelcomeController updated

--- src/main/java/hu/unideb/omlete/controller/WelcomeController.java
+++ src/main/java/hu/unideb/omlete/controller/WelcomeController.java
@@ -9,5 +9,19 @@ import javafx.scene.control.Button;
 import java.io.IOException;
 
 public class WelcomeController {
+    @FXML
+    private Button categoryButton;
 
+    @FXML
+    private Button createShoppingList;
+
+    @FXML
+    void switchToCategory(ActionEvent event) throws IOException {
+        App.setRoot("food_by_category.fxml");
+    }
+
+    @FXML
+    void switchToShoppingList(ActionEvent event) throws IOException {
+        App.setRoot("create_shopping_list.fxml");
+    }
 }
