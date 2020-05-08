8110be0f8b525dee34010cc591494ff02f748e04
zsigmondO <zsigmondo9000@gmail.com>
2020-05-08 18:44:01 +0200

    added new features

--- src/main/java/hu/unideb/omlete/main/App.java
+++ src/main/java/hu/unideb/omlete/main/App.java
@@ -19,7 +19,7 @@ public class App extends Application {
     @Override
     public void start(Stage stage){
         try {
-            scene = new Scene(loadFXML("food_by_category"));
+            scene = new Scene(loadFXML("welcome"));
             scene.getStylesheets().add(App.class.getResource("/view/styles.css").toExternalForm());
             stage.setScene(scene);
             stage.show();
